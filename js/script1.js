aba44e3728c7e4c8d0e16573b6eee55df0140f8c
Tevin Joel Aduma <tevin74@live.com>
2017-03-01 16:04:09 +0300

    add new variable for html and body

--- js/script1.js
+++ js/script1.js
@@ -1,23 +1,27 @@
-;(function ($) {
-  'use strict';
-  var content  = $('#main').smoothState({
-        // onStart runs as soon as link has been activated
-        onStart : {
+;
+(function($) {
+    'use strict';
+    var $body = $('html, body'), // Define jQuery collection
+        var content = $('#main').smoothState({
+            // onStart runs as soon as link has been activated
+            onStart: {
 
-          // Set the duration of our animation
-          duration: 250,
+                // Set the duration of our animation
+                duration: 250,
 
-          // Alterations to the page
-          render: function () {
+                // Alterations to the page
+                render: function() {
 
-            // Quickly toggles a class and restarts css animations
-            content.toggleAnimationClass('is-exiting');
+                    // Quickly toggles a class and restarts css animations
+                    content.toggleAnimationClass('is-exiting');
 
-            // Scroll user to the top
-                       $body.animate({ 'scrollTop': 0 });
+                    // Scroll user to the top
+                    $body.animate({
+                        'scrollTop': 0
+                    });
 
 
-          }
-        }
-      }).data('smoothState'); // makes public methods available
+                }
+            }
+        }).data('smoothState'); // makes public methods available
 })(jQuery);
